d9361c1b421c90dd29d1b661821c77689d796a93
timgdavies <tim@practicalparticipation.co.uk>
2015-05-31 20:37:41 -0400

    First version of scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,47 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import requests
+import json
+import scraperwiki
+from datetime import date, timedelta
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+limit = 0
+api_url = 'https://beta-api.contractfinder2.com'
+
+def search_notices(s_type ='Contract',s_status=['Open'],s_publishedFrom=None,s_publishedTo=None):
+    """Query the search endpoint and return a list of notices. Documentation of options available from API URL at /api/Help/Api/POST-Searches-Search"""
+    search_object = {'searchCriteria':{}}
+    if s_type:
+        search_object['searchCriteria']['type'] = s_type
+    if s_status:
+        search_object['searchCriteria']['statuses'] = s_status
+    if s_publishedFrom:
+        search_object['searchCriteria']['publishedFrom'] = s_publishedFrom
+    if s_publishedTo:
+        search_object['searchCriteria']['publishedFrom'] = s_publishedTo
+    print(search_object)
+    
+    r = requests.post(api_url+'/Searches/Search',data=json.dumps(search_object),headers={'content-type': 'application/json'})
+    return r.json()
+
+from_date = str(date.today() - timedelta(days=3))
+contract_list = search_notices(s_type ='Contract',s_status=['Open'],s_publishedFrom=from_date)
+print "Total results found: " + str(contract_list['hitsCount'])
+
+for notice in contract_list['noticeList']:
+    limit += 1
+    if limit > 9999:
+        break
+        
+    notice_id = notice['item']['id']
+    
+    print api_url+'/Published/Notice/OCDS/'+str(notice_id)
+    r = requests.get(api_url+'/Published/Notice/OCDS/'+notice_id,headers={'content-type': 'application/json'})
+    if r.status_code == 200:
+        release = r.json()
+        release_id = str(release['releases'][0]['id'])
+        scraperwiki.sqlite.save(unique_keys=['id'], data={"id": release_id, "data": json.dumps(release)})
+    else:
+        # We should extend the scraper to log failed notices somewhere
+        print "Error fetching " + notice_id
